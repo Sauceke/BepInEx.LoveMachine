b7ffd4a08e87a31245c192584cd4de268bbb8a97
Sauceke <saucekedesu@protonmail.com>
2024-06-24 23:10:55 +0300

    [OA] Upgrade to Our Apartment v0.5.3

--- src/LoveMachine.OA/Plugin.cs
+++ src/LoveMachine.OA/Plugin.cs
@@ -2,9 +2,8 @@
 using LoveMachine.Core;
 using LoveMachine.Core.NonPortable;
 
-namespace LoveMachine.OA
-{
-    [BepInPlugin(Globals.GUID, Globals.PluginName, Globals.Version)]
-    internal class Plugin : LoveMachinePlugin<OurApartmentGame>
-    { }
-}
+namespace LoveMachine.OA;
+
+[BepInPlugin(Globals.GUID, Globals.PluginName, Globals.Version)]
+internal class Plugin : LoveMachinePlugin<OurApartmentGame>
+{ }
