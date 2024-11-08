510a9f7ccb81e94876f2caa3e45a336a07b41d6b
Sauceke <saucekedesu@protonmail.com>
2024-11-09 01:18:36 +0300

    [KFC2] WIP

---
+++ src/LoveMachine.KFC2/KinkyFightClub2Adapter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Reflection;
+using HarmonyLib;
+using LoveMachine.Core.Common;
+using LoveMachine.Core.Game;
+using UnityEngine;
+
+namespace LoveMachine.KFC2;
+
+public class KinkyFightClub2Adapter : GameAdapter
+{
+    private GameObject maleRoot;
+    private GameObject femaleRoot;
+    private Animator femaleAnimator;
+
+    protected override MethodInfo[] StartHMethods => new[]
+    {
+        AccessTools.Method("SexSystem, Assembly-CSharp:StartSex")
+    };
+    
+    protected override MethodInfo[] EndHMethods => new[]
+    {
+        AccessTools.Method("SexSystem, Assembly-CSharp:Start"),
+        AccessTools.Method("SexSystem, Assembly-CSharp:Escape")
+    };
+    
+    protected override Dictionary<Bone, string> FemaleBoneNames => new()
+    {
+        { Bone.Vagina, "stick" },
+        { Bone.Mouth, "MouthLower" },
+        { Bone.LeftHand, "hand_L" },
+        { Bone.RightHand, "hand_R" }
+    };
+    
+    protected override Transform PenisBase => FindBoneByPath(maleRoot, "stick");
+    protected override int AnimationLayer => 0;
+    protected override int HeroineCount => 1;
+    protected override int MaxHeroineCount => 1;
+    protected override bool IsHardSex => true;
+
+    protected override Animator GetFemaleAnimator(int girlIndex) => femaleAnimator;
+
+    protected override GameObject GetFemaleRoot(int girlIndex) => femaleRoot;
+
+    protected override string GetPose(int girlIndex) =>
+        GetAnimatorStateInfo(0).fullPathHash.ToString();
+
+    protected override bool IsIdle(int girlIndex) => false;
+
+    protected override IEnumerator UntilReady(object instance)
+    {
+        yield return new WaitForSeconds(5f);
+        maleRoot = GameObject.Find("Player/ArmatureFem_000/Global/Position");
+        femaleRoot = GameObject.Find("Enemy/ArmatureFem_000/Global/Position");
+        femaleAnimator = femaleRoot.transform.parent.parent.parent.GetComponent<Animator>();
+    }
+}
---
+++ src/LoveMachine.KFC2/Plugin.cs
@@ -0,0 +1,9 @@
+﻿using BepInEx;
+using LoveMachine.Core;
+using LoveMachine.Core.NonPortable;
+
+namespace LoveMachine.KFC2;
+
+[BepInPlugin(Globals.GUID, Globals.PluginName, Globals.Version)]
+internal class Plugin : LoveMachinePlugin<KinkyFightClub2Adapter>
+{ }
