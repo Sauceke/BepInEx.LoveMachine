2d7689711eb0db2ae9ecdfe0f784806e120c3ad2
Sauceke <saucekedesu@protonmail.com>
2025-01-30 20:26:55 +0300

    [SIH] Update code to support 1.00

--- src/LoveMachine.SIH/SummerInHeatGame.cs
+++ src/LoveMachine.SIH/SummerInHeatGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,14 +14,14 @@ namespace LoveMachine.SIH
     {
         private static readonly int[] activeSteps = { 1, 2, 5 };
 
-        private GameObject femaleRoot;
-        private Animator femaleAnimator;
+        private GameObject[] femaleRoots;
+        private Animator[] femaleAnimators;
         private Traverse<int> step;
 
         protected override MethodInfo[] StartHMethods => new[]
         {
-            AccessTools.Method("FH_Controller, Assembly-CSharp:Start"),
-            AccessTools.Method("Talk_Controller, Assembly-CSharp:Start")
+            AccessTools.Method("FH_Controller, Assembly-CSharp:Start_Method"),
+            AccessTools.Method("Talk_Controller, Assembly-CSharp:Start_Method")
         };
 
         protected override MethodInfo[] EndHMethods =>
@@ -29,6 +30,7 @@ namespace LoveMachine.SIH
         protected override Dictionary<Bone, string> FemaleBoneNames => new Dictionary<Bone, string>
         {
             { Bone.Vagina, "HS_cli" },
+            { Bone.Anus, "HS_anaruB01" },
             { Bone.LeftHand, "bip01 L Finger1Nub" },
             { Bone.RightHand, "bip01 R Finger1Nub" },
             { Bone.Mouth, "HS_mouth01" },
@@ -38,18 +40,24 @@ namespace LoveMachine.SIH
             { Bone.RightFoot, "bip01 R Toe4Nub" }
         };
 
-        protected override Transform PenisBase => GameObject.Find("HS00_penis01_PC01").transform;
+        protected override Transform PenisBase => throw new NotImplementedException();
+
+        protected override Transform[] PenisBases =>
+            new[] { GameObject.Find("HS01_penis11_PC01"), GameObject.Find("HS00_penis01_PC03") }
+                .Where(go => go != null && go.activeInHierarchy)
+                .Select(go => go.transform).ToArray();
+
         protected override int AnimationLayer => 0;
-        protected override int HeroineCount => 1;
-        protected override int MaxHeroineCount => 1;
+        protected override int HeroineCount => femaleRoots.Length;
+        protected override int MaxHeroineCount => 2;
         protected override bool IsHardSex => true;
 
-        protected override Animator GetFemaleAnimator(int girlIndex) => femaleAnimator;
+        protected override Animator GetFemaleAnimator(int girlIndex) => femaleAnimators[girlIndex];
 
-        protected override GameObject GetFemaleRoot(int girlIndex) => femaleRoot;
+        protected override GameObject GetFemaleRoot(int girlIndex) => femaleRoots[girlIndex];
 
         protected override string GetPose(int girlIndex) =>
-            femaleAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash.ToString();
+            femaleAnimators[girlIndex].GetCurrentAnimatorStateInfo(0).fullPathHash.ToString();
 
         protected override bool IsIdle(int girlIndex) => !activeSteps.Contains(step.Value);
 
@@ -59,8 +67,16 @@ namespace LoveMachine.SIH
         {
             yield return new WaitForSeconds(5f);
             var controller = Traverse.Create(instance);
-            femaleRoot = GameObject.Find("CH_Prefub_A/CHbase");
-            femaleAnimator = femaleRoot.GetComponent<Animator>();
+            femaleRoots = new[]
+                {
+                    GameObject.Find("CH_Prefub_A/CHbase"),
+                    GameObject.Find("CH_Prefub_B/CHbase")
+                }
+                .Where(go => go != null && go.activeInHierarchy)
+                .ToArray();
+            femaleAnimators = femaleRoots
+                .Select(root => root.GetComponent<Animator>())
+                .ToArray();
             step = instance.GetType().Name == "FH_Controller"
                 ? controller.Field<int>("FH_Step")
                 : controller.Field<int>("TK_Step");
